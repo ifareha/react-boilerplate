21bccd85d6752af3b640f5d103e8d51e04685960
ifareha <khanfareha51@gmail.com>
2024-10-01 18:22:32 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='w-full relative h-screen text-white flex items-center justify-center bg-red-500'>
+      <div className='w-[35%] h-full bg-black'>
+        <div className='flex items-center justify-center h-[9%]'>
+          <h1 className='text-[1.4vw] font-semibold '>PROTOFILO.</h1>
+        </div>
+        <div className='absolute top-[65%] left-[16%]'>
+          <h1 className='text-[3vw] leading-[0.1]'>MY NAME IS</h1>
+          <h1 className='font-bold text-[3.5vw] tracking-wide '>ABDULREHMAN</h1>
+          <button className='bg-yellow-500 py-1 px-7 text-[2vw] tracking-tighter text-black font-bold'>I'M A UI DEVELOPER</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='w-[65%] h-full'>
+      <div className='flex items-center z-[22] font-semibold justify-center gap-9 h-[9%] bg-zinc-800 tracking-wide'>
+          <h1 className='text-[1vw] font-semibold underline decoration-2'>HOME</h1>
+          <h1 className='text-[1vw] font-semibold '>About</h1>
+          <h1 className='text-[1vw] font-semibold '>WORKS</h1>
+          <h1 className='text-[1vw] font-semibold '>BLOG</h1>
+          <h1 className='text-[1vw] font-semibold '>CONTACT</h1>
+        </div>
+        <img className='h-full w-full object-cover' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQZD0wF9u6XzIrzQaRd14zlPDZkvMEqUJJ1g&s" alt="" />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div></div>
+    </div>
   )
 }
-
-export default App
---
+++ src/Products.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+ function Products({data}) {
+  return (
+    <div className='flex'>
+          <h1 className='text-[1vw] font-semibold '>HOME</h1>
+          <h1 className='text-[1vw] font-semibold '>About</h1>
+          <h1 className='text-[1vw] font-semibold '>WORKS</h1>
+          <h1 className='text-[1vw] font-semibold '>BLOG</h1>
+          <h1 className='text-[1vw] font-semibold '>CONTACT</h1>
+          </div>
+  )
+}
+
+export default Products;
